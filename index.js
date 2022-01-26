0c71b7718ab14a0f97eb664f7d968299718ae006
Vinicius Linck <vinicius.linck@clevertech.biz>
2022-01-26 18:01:05 -0300

    added express server bootstrap

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const config = require('config');
 const SandLogger = require('./lib/logger')
+const app = require('./lib/app');
 
 const name  = config.get('app.name');
+const port = config.get('app.port');
 
 const logger = SandLogger.create(
   config.get('app.logging.level'),
   name,
 );
 
-logger.info(`Starting ${name}...`);
+logger.info('Starting http server...');
+
+module.exports = app.run(port)
+  .then(() => logger.info(`Running on port: ${port}`))
+  .catch(err => {
+    logger.error(err, 'Something went wrong when trying to run the app!!');
+    process.exit(-1);
+  });
+

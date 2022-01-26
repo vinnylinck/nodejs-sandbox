de6bebcfc064cc6acf81c71523494a7fa92ac12a
Vinicius Linck <vinicius.linck@clevertech.biz>
2022-01-26 18:48:23 -0300

    fixed lint issues

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const config = require('config');
-const SandLogger = require('./lib/logger')
+const SandLogger = require('./lib/logger');
 const app = require('./lib/app');
 
-const name  = config.get('app.name');
+const name = config.get('app.name');
 const port = config.get('app.port');
 
 const logger = SandLogger.create(
@@ -14,8 +14,7 @@ logger.info('Starting http server...');
 
 module.exports = app.run(port)
   .then(() => logger.info(`Running on port: ${port}`))
-  .catch(err => {
+  .catch((err) => {
     logger.error(err, 'Something went wrong when trying to run the app!!');
     process.exit(-1);
   });
-

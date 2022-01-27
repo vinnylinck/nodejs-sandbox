a7e317c5fe247050284e75796146dd3d2239f2fb
Vinicius Linck <vinicius.linck@clevertech.biz>
2022-01-27 14:10:53 -0300

    fixed eslint issues

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const config = require('config');
+const SandApp = require('./lib/app');
 const SandLogger = require('./lib/logger');
-const app = require('./lib/app');
 
 const name = config.get('app.name');
 const port = config.get('app.port');
+const dburl = config.get('db.url');
 
 const logger = SandLogger.create(
   config.get('app.logging.level'),
   name,
 );
 
+// bootstrapping
+const app = new SandApp(port, dburl);
+
 logger.info('Starting http server...');
 
-module.exports = app.run(port)
+module.exports = app.Run()
   .then(() => logger.info(`Running on port: ${port}`))
   .catch((err) => {
     logger.error(err, 'Something went wrong when trying to run the app!!');

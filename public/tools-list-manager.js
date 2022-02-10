3dac79d740b303f164e4026f00ca6180bd7b1c08
Vinicius Linck <vinnylinck@gmail.com>
2022-02-10 11:52:55 -0300

    delete list

--- public/tools-list-manager.js
+++ public/tools-list-manager.js
@@ -34,3 +34,18 @@ function toggleItem(iid, value) {
     }
   });
 }
+
+// eslint-disable-next-line no-unused-vars
+function deleteList(lid) {
+  const uri = `${window.location.href}/${lid}`;
+  const token = document.getElementById('token').value;
+
+  fetch(uri, { method: 'DELETE', headers: { 'XSRF-TOKEN': token } })
+    .then((res) => {
+      if (res.ok) {
+        window.location.reload(true);
+      } else {
+        alert(res.statusText);// eslint-disable-line no-alert
+      }
+    });
+}

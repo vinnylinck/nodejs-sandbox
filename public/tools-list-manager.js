0fc44c9fdd27f8b32c0cffeb33662df6e23135bd
Vinicius Linck <vinnylinck@gmail.com>
2022-02-10 13:34:21 -0300

    edit list

--- public/tools-list-manager.js
+++ public/tools-list-manager.js
@@ -49,3 +49,25 @@ function deleteList(lid) {
       }
     });
 }
+
+// eslint-disable-next-line no-unused-vars
+function updateList() {
+  const uri = `${window.location.href}`;
+  const token = document.getElementById('token').value;
+  const name = document.getElementById('list-name').value;
+
+  fetch(
+    uri,
+    {
+      method: 'PUT',
+      headers: { 'XSRF-TOKEN': token, 'Content-Type': 'application/json' },
+      body: JSON.stringify({ name }),
+    },
+  ).then((res) => {
+    if (res.ok) {
+      document.getElementById('back-link').click();
+    } else {
+      alert(res.statusText);// eslint-disable-line no-alert
+    }
+  });
+}

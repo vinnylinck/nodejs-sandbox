b3577ce3bfab8868e50e8eea7d3c7a9f9c40dada
Vinicius Linck <vinnylinck@gmail.com>
2022-03-06 21:05:31 -0300

    removing broken command

--- services/common/security/index.js
+++ services/common/security/index.js
@@ -1,6 +1,7 @@
 module.exports = {
   scopes: {
     LIST_READ: 'list.read',
+    LIST_WRITE: 'list.write',
     ITEM_READ: 'item.read',
     ITEM_WRITE: 'item.write',
     EVENT_READ: 'event.read',
